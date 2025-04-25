2a68a2ad709acd0f3ba182ec7c9ab04dea4cf98b
hponemyatmin <hponemyatmin@my.jcu.edu.au>
2025-04-25 02:32:47 +0100

    Added Library Account Entity

---
+++ src/main/java/uk/ac/leedsbeckett/hmm/library_portal/entities/LibraryAccount.java
@@ -0,0 +1,21 @@
+package uk.ac.leedsbeckett.hmm.library_portal.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+public class LibraryAccount {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String studentId;
+
+    private String pin;
+
+    private boolean isUpdated;     // check if the user first login/ not
+
+}
