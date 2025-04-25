21cfd6eeb80d5904c766920bcd1a25c9b6c941c9
hponemyatmin <hponemyatmin@my.jcu.edu.au>
2025-04-25 01:48:40 +0100

    Initialize the Project and add dependencies

---
+++ src/main/java/uk/ac/leedsbeckett/hmm/library_portal/LibraryPortalApplication.java
@@ -0,0 +1,13 @@
+package uk.ac.leedsbeckett.hmm.library_portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryPortalApplication.class, args);
+	}
+
+}
