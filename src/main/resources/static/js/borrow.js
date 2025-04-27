fe7b4f0b6673af2a0c1246be0f546f74746cfd21
hponemyatmin <hponemyatmin@my.jcu.edu.au>
2025-04-27 04:12:22 +0100

    Added borrow page

---
+++ src/main/resources/static/js/borrow.js
@@ -0,0 +1,50 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const studentId = localStorage.getItem("studentId");
+
+    if (!studentId) {
+        alert("Please log in first.");
+        window.location.href = "login.html";
+        return;
+    }
+
+    if (studentId === "admin") {
+        document.getElementById("adminNav").style.display = "inline";
+    } else {
+        document.getElementById("studentNav").style.display = "inline";
+    }
+
+    document.getElementById("borrowForm").addEventListener("submit", async (e) => {
+        e.preventDefault();
+
+        const isbn = document.getElementById("bookIsbn").value.trim();
+        if (!isbn) {
+            document.getElementById("message").textContent = "Please enter an ISBN.";
+            return;
+        }
+
+        try {
+            const res = await fetch(`http://localhost:8082/history/${studentId}/borrow/${isbn}`, {
+                method: "PUT"
+            });
+
+            if (!res.ok) {
+                throw new Error("Failed to borrow book. Book may not exist or no copies available.");
+            }
+
+            localStorage.setItem("borrowIsbn", isbn);
+            window.location.href = "myaccount.html"
+
+            // document.getElementById("message").textContent = "Book borrowed successfully!";
+            // document.getElementById("borrowForm").reset();
+
+        } catch (error) {
+            console.error(error);
+            document.getElementById("message").textContent = error.message;
+        }
+    });
+});
+
+function signOut() {
+    localStorage.removeItem("studentId");
+    window.location.href = "login.html";
+}
