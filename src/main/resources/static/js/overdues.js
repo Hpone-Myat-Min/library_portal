990e96cea8b1238103846b1501990b2a3a370537
hponemyatmin <hponemyatmin@my.jcu.edu.au>
2025-04-28 23:43:57 +0100

    Added overdue loans page for admin

--- src/main/resources/static/js/overdues.js
+++ src/main/resources/static/js/overdues.js
@@ -1,19 +1,19 @@
 document.addEventListener("DOMContentLoaded", async () => {
     try {
-        const res = await fetch("http://localhost:8082/admin/loans/current");
+        const res = await fetch("http://localhost:8082/admin/loans/overdue");
         const transactions = await res.json();
-        const tbody = document.querySelector("#loansTable tbody");
+        const tbody = document.querySelector("#overduedtable tbody");
         tbody.innerHTML = "";
 
         transactions.forEach(t => {
-                const row = document.createElement("tr");
-                row.innerHTML = `
+            const row = document.createElement("tr");
+            row.innerHTML = `
                     <td>${t.book.isbn}</td>
                     <td>${t.book.title}</td>
                     <td>${t.studentId}</td>
                     <td>${t.dateBorrowed}</td>
                 `;
-                tbody.appendChild(row);
+            tbody.appendChild(row);
 
         });
 
