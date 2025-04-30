0df2058ecb1339f05ec8f23d6bbaacf519fcc085
hponemyatmin <hponemyatmin@my.jcu.edu.au>
2025-04-30 21:51:03 +0100

    Fix the type in nav bar to direct to addBook page

---
+++ src/test/java/uk/ac/leedsbeckett/hmm/library_portal/services/TransactionServiceImplTest.java
@@ -0,0 +1,33 @@
+package uk.ac.leedsbeckett.hmm.library_portal.services;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class TransactionServiceImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void borrowBook() {
+    }
+
+    @Test
+    void returnBook() {
+    }
+
+    @Test
+    void getTransaction() {
+    }
+
+    @Test
+    void getStudentTransaction() {
+    }
+}
